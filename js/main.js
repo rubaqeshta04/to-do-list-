acd87538a28c98e1f387b40c2c2c6b2c9f7f2586
rubaqeshta04 <qeshtaruba@gmail.com>
2025-09-06 16:24:36 +0300

    Create add todo modal #6

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
- const openBtn = document.getElementById("openModal");
+const openBtn = document.getElementById("openModal");
     const closeBtn = document.getElementById("closeModal");
     const overlay = document.getElementById("overlay");
-    const addBtn = document.getElementById("addBtn");
-    const todoInput = document.getElementById("todoInput");
-    const todoList = document.getElementById("todoList");
-
     // Open modal
     openBtn.addEventListener("click", () => {
       overlay.style.display = "flex";
@@ -22,13 +18,13 @@
       }
     });
 
-    // Add new task
-    addBtn.addEventListener("click", () => {
-      const task = todoInput.value.trim();
-      if (task !== "") {
-        const li = document.createElement("li");
-        li.textContent = task;
-        todoList.appendChild(li);
-        todoInput.value = "";
-      }
-    });
+    const addTodoBtn = document.getElementById('add-to-do-btn')
+    const addToDoModal = document.getElementById('add-todo')
+    const closeAddTodoModal = document.getElementById('closeAddTodoModal')
+    addTodoBtn.addEventListener("click", () => {
+      addToDoModal.style.display = "flex"; 
+    })
+    closeAddTodoModal.addEventListener('click' , () => {
+      addToDoModal.style.display ="none";
+    
+    })
